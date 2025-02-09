ec46f7064e33b08e026ae20ada98948c6bb0969f
Prince Otchere <otchereprince00@gmail.com>
2025-02-09 14:11:11 -0500

    Update lang.js
    Default root path to index page

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -18,7 +18,7 @@ const urls = {
        work: 'work_zh.html',
        contact: 'contact_zh.html'
     },
-       // Add more languages as needed
+   // Add more languages as needed
 };
 
 document.getElementById('languageSwitcher').addEventListener('change', function() {
@@ -30,28 +30,29 @@ document.getElementById('languageSwitcher').addEventListener('change', function(
    
    // Determine the page name without the language suffix
    const pageName = currentPage.replace(/(_zh|_es|_en)?\.html$/, '');
-   
+      
    console.log('Page Name:', pageName);
    
    if (urls[selectedLanguage] && urls[selectedLanguage][pageName]) {
-       window.location.href = urls[selectedLanguage][pageName];
+      window.location.href = urls[selectedLanguage][pageName];
    } else {
-       console.error('URL not found for selected language and page.');
+      console.error('URL not found for selected language and page.');
    }
-   });
-   
-    document.getElementById('languageSwitcher').addEventListener('change', function() {
-       const selectedLanguage = this.value;
-       const currentPage = window.location.pathname.split('/').pop();
-    
-       if (currentPage.includes('index')) {
-          window.location.href = urls[selectedLanguage].index;
-       } else if (currentPage.includes('about')) {
-          window.location.href = urls[selectedLanguage].about;
-       } else if (currentPage.includes('work')) {
-          window.location.href = urls[selectedLanguage].work;
-       } else if (currentPage.includes('contact')) {
-          window.location.href = urls[selectedLanguage].contact;
-       }
-    });
-    
+
+});
+
+document.getElementById('languageSwitcher').addEventListener('change', function() {
+   const selectedLanguage = this.value;
+   const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+
+   if (currentPage.includes('index')) {
+      window.location.href = urls[selectedLanguage].index;
+   } else if (currentPage.includes('about')) {
+      window.location.href = urls[selectedLanguage].about;
+   } else if (currentPage.includes('work')) {
+      window.location.href = urls[selectedLanguage].work;
+   } else if (currentPage.includes('contact')) {
+      window.location.href = urls[selectedLanguage].contact;
+   }
+
+});
